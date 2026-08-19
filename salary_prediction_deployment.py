2120b8e954559cff290fb12a8508199bdfbf6e0b
Alvin Nelson <158695662+RONIN-A6@users.noreply.github.com>
2026-08-19 10:55:09 +0530

    updated age and exp range and solved bracket error

--- salary_prediction_deployment.py
+++ salary_prediction_deployment.py
@@ -17,11 +17,11 @@ encoder = joblit.load("label_encoder.pkl")
 
 st.title("Salary Prediction")
 
-age = st.number_input("Age")
+age = st.number_input("Age",18,65)
 gender = st.selectbox("Gender",encoder["Gender"].classes_)
 education_level = st.selection("Education Level",encoder["Education Level"].classes_)
 job_title = st.selection("Job Title",encoder["Job Title"].classes_)
-years_of_exp = st.number_input("Years of Experience")
+years_of_exp = st.number_input("Years of Experience",0,40)
 
 df = pd.DataFrame((
     "Age":[age],
@@ -33,7 +33,7 @@ df = pd.DataFrame((
 
 if st.button("Predict Salary"):
   for col in encoder:
-    df(col) = encoder[col].transform(df[col])
+    df[col] = encoder[col].transform(df[col])
 
   prediction = model.predict(df)
   st.success(f"Predicted Salary : {prediction[0],2f}")
